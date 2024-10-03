039a9f650190f5ccf5584049f597e0edf94e0dcc
C0dewithLokesh <lokeshkaushik2243@gmail.com>
2024-10-03 20:30:48 +0530

    initial schema and signup route done

---
+++ medium-backend/prisma/migrations/20241003132623_init_schema/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "thumbnail" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "autorId" TEXT NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Blog" ADD CONSTRAINT "Blog_autorId_fkey" FOREIGN KEY ("autorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- medium-backend/src/index.ts
+++ medium-backend/src/index.ts
@@ -1,9 +1,64 @@
-import { Hono } from 'hono'
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import bcrypt from "bcryptjs";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use("*", async (c, next) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  c.set("prisma" as never, prisma);
+  await next();
+});
 
-export default app
+app.post("/api/v1/user/signup", async (c) => {
+  const prisma = c.get("prisma" as never) as PrismaClient;
+  const body = await c.req.json();
+
+  // Hashing the password before saving it
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(body.password, saltRounds);
+
+  try {
+    await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password: hashedPassword,
+      },
+    });
+    return c.text("User signed up successfully!");
+  } catch (error) {
+    c.status(411);
+    console.log(error);
+    return c.text("User already exist with this username or email");
+  }
+});
+
+app.post("/api/v1/user/signin", async (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog:id", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
